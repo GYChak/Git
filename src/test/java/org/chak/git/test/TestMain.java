06f3f6b53e36531a9f614e8eac9f05f7e17ed06b
chak <851558146@qq.com>
2023-06-07 18:36:02 +0800

    第二次提交

--- src/test/java/org/chak/git/test/TestMain.java
+++ src/test/java/org/chak/git/test/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
 
     public static void main(String[] args) {
         System.out.println("Hello, Git in IDEA!");
+        System.out.println("Hello, Git in IDEA!1");
     }
 
 }
