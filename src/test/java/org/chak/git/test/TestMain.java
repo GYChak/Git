6109c6a0db1e00dd89c481cbf9ff7abe542bd8d8
chak <851558146@qq.com>
2023-06-07 18:50:51 +0800

    第三次提交

--- src/test/java/org/chak/git/test/TestMain.java
+++ src/test/java/org/chak/git/test/TestMain.java
@@ -3,8 +3,14 @@ package org.chak.git.test;
 public class TestMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Git in IDEA!");
-        System.out.println("Hello, Git in IDEA!1");
+        // 第一次提交
+        System.out.println("Hello,Git in IDEA!haha~~");
+        // 第二次提交
+        System.out.println("Hello,Git in IDEA!1");
+        // 第三次不经过暂存区，直接提交
+        System.out.println("Hello,Git in IDEA!1");
+        // 在 hot-fix1 分支上添加的内容
+        System.out.println("Hello,Git in IDEA!");
     }
 
 }
